067d358bd393985e36f9e9e4358b51d06ba5f1a5
ThiagoMaia1 <tthiagopmaia@gmail.com>
2021-03-23 21:51:02 -0300

    import paper

--- src/styles/PaperGradient.ts
+++ src/styles/PaperGradient.ts
@@ -1,9 +1,10 @@
-export default function getGradient(rectangle : paper.Rectangle, colors : Array<string>) {
-    return {
-        gradient: {
-            stops: colors,
-        },
-        origin: rectangle.topLeft,
-        destination: rectangle.bottomRight,
-    }
+import { Color, Gradient, GradientStop } from 'paper/dist/paper-core';
+
+export default function getGradient(rectangle : paper.Rectangle, colors : Array<string>) : paper.Color {
+    let gradient = new Gradient();
+    gradient.stops = colors.map(c => new GradientStop(new Color(c)));
+    return new Color(gradient,
+        rectangle.topLeft,
+        rectangle.bottomRight,
+    );
 }
