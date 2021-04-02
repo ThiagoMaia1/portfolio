3b9b1c96a89f40bdecec03010ced6c185e96cf98
ThiagoMaia1 <tthiagopmaia@gmail.com>
2021-04-02 15:39:40 -0300

    Made Skills list responsive

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,6 +3,8 @@ import SectionTitle from "../basic/SectionTitle/SectionTitle";
 import technologies from '../../models/Technology/Technologies';
 import TechnologyLink from "../basic/TechnologyLink/TechnologyLink";
 import './Skills.scss';
+import { useState } from "react";
+import useWindowResize from "../../hooks/useWindowResize";
 
 const t = technologies;
 
@@ -12,6 +14,7 @@ const skills = [
     t.Javascript, 
     t.Typescript, 
     t.GitHub,
+    t.Angular,
     t.ReactJs, 
     t.ReactNative, 
     t.Firebase,
@@ -26,12 +29,23 @@ const skills = [
 
 function Skills() {
 
+    const columnWidth = 350;
+    
+    const getNumberOfColumns = (width : number) => Math.floor(width/columnWidth);
+
+    let [numberOfColumns, setNumberOfColumns] = useState(getNumberOfColumns(window.innerWidth));
+    useWindowResize(size => setNumberOfColumns(getNumberOfColumns(size.width)));
+
     return (
         <div>
             <SectionTitle text={getTranslatedSentence('skills')} />
             <div className='skill-list-container'>
-                {skills.map(s => 
-                    <TechnologyLink technology={s}/>
+                {getGroupedSkills(numberOfColumns).map(g =>
+                    <div key={g[0].name}>
+                        {g.map(s => 
+                            <TechnologyLink key={s.name} technology={s}/>
+                        )}
+                    </div>
                 )}
             </div>
         </div>
@@ -39,3 +53,15 @@ function Skills() {
 }
 
 export default Skills;
+function getGroupedSkills(numberOfGroups : number) {
+    console.log(numberOfGroups);
+    const step = skills.length/numberOfGroups;
+    const groups = [];
+    //Reversions necessary to get the first column to be the longest.
+    const skillsReversed = [...skills]; 
+    skillsReversed.reverse();
+    for (let i = 0; i < skills.length; i += step)
+        groups.push(skillsReversed.slice(i, i + step).reverse());
+    return groups.reverse();
+}
+
