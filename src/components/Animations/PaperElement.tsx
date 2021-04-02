cce4ed1390c9b7243cb3531c81fa08d45fcf7ffb
ThiagoMaia1 <tthiagopmaia@gmail.com>
2021-04-02 15:34:06 -0300

    Resetting paper.js animations on window resize

--- src/components/Animations/PaperElement.tsx
+++ src/components/Animations/PaperElement.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import paper from 'paper';
 import './PaperElement.scss';
 import useWindowResize from '../../hooks/useWindowResize';
@@ -13,14 +13,24 @@ export default function PaperElement({animation, children = null}
     : {animation : (scope : paper.PaperScope) => void, children ?: React.ReactNode}) {
 
     const [canvasId, setCanvasId] = useState(uniqueCanvasId());
+    const timeout = useRef(setTimeout(() => void 0));
+    const scopeRef = useRef<paper.PaperScope>();
 
     useWindowResize(() => setCanvasId(uniqueCanvasId()));
     
-    useEffect(function () {setTimeout(() => {
-        let scope = new paper.PaperScope();
-        scope.setup(canvasId);
-        animation(scope);
-    }, 0)}, [canvasId]);
+    useEffect(function () {
+        clearTimeout(timeout.current);
+        timeout.current = setTimeout(() => {
+            if (scopeRef.current !== undefined) 
+                scopeRef.current.projects.forEach(p => p.remove());
+            scopeRef.current = new paper.PaperScope();
+            let scope = scopeRef.current;
+            if (!document.querySelector(`#${canvasId}`)) 
+                return;
+            scope.setup(canvasId);
+            animation(scope);
+        }, 0)
+    }, [canvasId]);
 
     return <div className='paper-container'>
         <div className='paper-children-container'>
