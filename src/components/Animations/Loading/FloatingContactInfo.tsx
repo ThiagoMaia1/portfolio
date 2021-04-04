c5a962dc9f3f38c6dc5ae7bb5a6b3396baaeeb34
ThiagoMaia1 <tthiagopmaia@gmail.com>
2021-04-04 10:13:09 -0300

    Added floating contact info (resolved #5)

---
+++ src/components/Animations/Loading/FloatingContactInfo.tsx
@@ -0,0 +1,14 @@
+import ContactInfo from '../../basic/ContactInfo/ContactInfo';
+import './FloatingContactInfo.scss';
+
+function FloatingContactInfo() {
+
+    return (
+        <div className='floating-contact-info-container'>
+            <ContactInfo includeDevelopedBy={false}/>
+        </div>
+    )
+}
+
+export default FloatingContactInfo;
+                
--- src/components/Animations/Loading/Loading.tsx
+++ src/components/Animations/Loading/Loading.tsx
@@ -1,66 +1,94 @@
-import React from 'react'
 import PaperElement from '../PaperElement'
 import getGradient from '../PaperGradient';
 import './Loading.scss';
 import { fixedStar } from '../../../constants/Constants';
+import React, { useRef } from 'react';
+import FloatingContactInfo from './FloatingContactInfo';
 
 function Loading() {
-    return (<div className='loading-container'>
-        <PaperElement animation={(scope : paper.PaperScope) => {
-            scope.activate();
-            const view = scope.project.view;
-            const radius = view.size.width*0.4;
-            let { smoothing, numberOfPoints } = fixedStar;
+    const proportionOfStar = 0.2;
+    const ref = useRef<HTMLDivElement>(null);
+    const recProportionOfCanvas = 0.7;
+    const closedRectangleHeight = 10;
+    const changeSizeSpeed = 6;
 
-            var path = new scope.Path.Star(view.center, numberOfPoints, radius, radius);
+    return (
+        <div className='fixed-loading'>
+            <div className='loading-container'>
+                <PaperElement animation={(scope : paper.PaperScope) => {
+                    scope.activate();
+                    const view = scope.project.view;
+                    const radius = view.size.width*proportionOfStar/2;
+                    const lengthStartCenter = radius*1.5;
+                    const starCenter = new scope.Point(lengthStartCenter, view.bounds.height - lengthStartCenter);
+                    let { smoothing, numberOfPoints } = fixedStar;
 
-            // var path = new scope.Path();
-            // path.selected = true;
-            path.fillColor = getGradient(path.bounds, ['#A60000', '#ff3c00'], scope);
+                    var path = new scope.Path.Star(starCenter, numberOfPoints, radius, radius);
+                    const openedRectangleHeight = view.bounds.height*recProportionOfCanvas;
+                    const diagonalFactor = 1;//view.bounds.width*recProportionOfCanvas/openedRectangleHeight;
+                    const recBaseLen = radius/2;
+                    const recBase = path.bounds.center.add(new scope.Point(-recBaseLen, recBaseLen));
+                    let rectangleHeight = closedRectangleHeight;
+                    const getRectangle = (height : number, radius : number) => 
+                        new scope.Path.Rectangle({
+                            rectangle: new scope.Rectangle(
+                                path.bounds.center.add(new scope.Point(height*diagonalFactor, -height)), 
+                                recBase,
+                            ), 
+                            radius,
+                            insert: false
+                        });
+                    const compoundPath = new scope.CompoundPath({
+                        children: [path, getRectangle(0, 0)], 
+                        fillColor: getGradient(path.bounds, ['#A60000', '#ff3c00'], scope)
+                    });
 
-            const initialSpeedRate = 1;
-            let speedRate = initialSpeedRate;
-            let interval : NodeJS.Timeout;
-            // let running = true;
-            let time = 0;
-            // let text = new scope.PointText(view.bounds.center);
-            // text.content = 'T';
-            // text.style.fontFamily = 'Roboto Slab';
-            // text.style.fontWeight = 900;
-            // text.style.fillColor = new scope.Color('white');
-            // text.style.fontSize = '3em';
-            // text.style.justification = 'center';
-            // text.position.y += text.bounds.height/3.3;
+                    let time = 0;
+                    let text = new scope.PointText(starCenter);
+                    text.content = 'T';
+                    text.style.fontFamily = 'Roboto Slab';
+                    text.style.fontWeight = 900;
+                    text.style.fillColor = new scope.Color('white');
+                    text.style.fontSize = '3em';
+                    text.style.justification = 'center';
+                    text.position.y += text.bounds.height/3.3;
 
-            view.onFrame = draw;
-            path.onMouseEnter = () => changeSpeed(0);
-            path.onMouseLeave = () => changeSpeed(initialSpeedRate);
+                    view.onFrame = draw;
 
-            function draw(event : any) {
-                // if (!running) return;
-                time += event.delta*speedRate;
-                scope.activate();
-                const variation : number = Math.sin(time*3)*radius/10; 
-                var newStar = new scope.Path.Star(view.center, numberOfPoints, radius - 2 + Math.abs(variation), radius - Math.abs(variation));
-                newStar.rotate(time*radius*1.5);
-                path.segments = newStar.segments;
-                smoothing(path);
-            }
+                    function draw(event : any) {
+                        time += event.delta;
+                        scope.activate();
+                        const variation : number = Math.sin(time*3)*radius/10; 
+                        var newStar = new scope.Path.Star(starCenter, numberOfPoints, radius - 2 + Math.abs(variation), radius - Math.abs(variation));
+                        newStar.rotate(time*radius*1.5);
+                        path.segments = newStar.segments;
+                        smoothing(path);
+                        if (ref.current) {
+                            ref.current.style.height = compoundPath.bounds.height + 'px';
+                            ref.current.style.width  = compoundPath.bounds.width + 'px';
+                        }
+                        let mouseIsOverCanvas = !!document.querySelector(`.${ref.current?.className}:hover`);
+                        changeSize(mouseIsOverCanvas, time);
+                    }
 
-            function changeSpeed(target : number) {
-                clearInterval(interval);
-                let direction = Math.sign(target - speedRate);
-                interval = setInterval(() => {
-                    speedRate += direction*0.01;
-                    if ((direction > 0 && speedRate > target) || (direction < 0 && speedRate < target)) clearInterval(interval);
-                }, 10);
-            }
+                    function changeSize(mouseIsOverCanvas : boolean, time : number) {
+                        const cycle = rectangleHeight > closedRectangleHeight ? Math.abs(Math.cos(time*2)) : 0;
+                        const rec = getRectangle(rectangleHeight, cycle*15 + rectangleHeight/8);
+                        rec.smooth({type: 'geometric', factor: cycle*0.3 + 0.3})
+                        compoundPath.children[1] = rec;
+                        let signal = 0;
+                        if (mouseIsOverCanvas && rectangleHeight < openedRectangleHeight) signal = 1;
+                        if (!mouseIsOverCanvas && rectangleHeight > closedRectangleHeight) signal = -1;
+                            rectangleHeight += signal*changeSizeSpeed;
+                    }
 
-        }}>
-            <div className='loading-T'>
-                <span>T</span>
+                }}>
+                    <div className='loading-gesture-detector' ref={ref}>
+                        <FloatingContactInfo/>
+                    </div>
+                </PaperElement>
             </div>
-        </PaperElement></div>
+        </div>
     )
 }
 
