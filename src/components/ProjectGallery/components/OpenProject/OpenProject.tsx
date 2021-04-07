daf41584e959669cc55af6eff9976add0ba20a05
ThiagoMaia1 <tthiagopmaia@gmail.com>
2021-04-06 21:54:22 -0300

    Added x too close the project (resolved #15)

--- src/components/ProjectGallery/components/OpenProject/OpenProject.tsx
+++ src/components/ProjectGallery/components/OpenProject/OpenProject.tsx
@@ -1,15 +1,25 @@
-import { LegacyRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useClosingAnimation from '../../../../hooks/useClosingAnimation';
-import useOutsideClick from '../../../../hooks/useOutsideClick';
 import Project from '../../../../models/Project/Project';
-import './OpenProject.scss';
 import ProjectCarrossel from '../ProjectCarrossel/ProjectCarrossel';
 import ProjectTextColumn from '../ProjectTextColumn/ProjectTextColumn';
 import useWindowDimensions from '../../../../hooks/useWindowDimensions';
+import './OpenProject.scss';
 
 const OpenProject = ({project, onClick} : {project : Project, onClick : () => void}) => {
 
     let [ativo, setAtivo] = useState(true);
+    let [scrollOutside, setScrollOutside] = useState(0);
+    let timeoutScroll = useRef(setTimeout(() => void 0, 0));
+    useEffect(() => {
+        clearTimeout(timeoutScroll.current);
+        if(Math.abs(scrollOutside) > 200) {
+            fechar();
+            setScrollOutside(0);
+        }
+        timeoutScroll.current = setTimeout(() => setScrollOutside(0), 2000)
+    }, [scrollOutside, timeoutScroll]);
+
     const time = 1000;
 
     const fechar = () => setAtivo(false);
@@ -22,21 +32,24 @@ const OpenProject = ({project, onClick} : {project : Project, onClick : () => vo
         time
     )
 
-    const ref = useOutsideClick(fechar);
-
     const opacityTransition = `opacity 1ms ease-in-out ${ativo ? time : 0}ms`;
     const {height, width} = useWindowDimensions();
     const isVertical = height > width;
 
-    return <div className='open-project-wrapper' ref={ref as unknown as LegacyRef<HTMLDivElement>}>
+    return <div className='open-project-wrapper'>
             <div className='shadow-open-project-animation'>
                 <div className='open-project project-card' 
                     style={{...style, transition: style.transition + ', ' + opacityTransition}}/>
             </div>
-            <div className='display-open-project' style={{opacity: 1 - style.opacity, transition: opacityTransition}}>
-                <div className='project-info-container'>
-                    <ProjectCarrossel project={project} isVertical={isVertical}/>
-                    <ProjectTextColumn project={project} isVertical={isVertical}/>
+            <div className='display-open-project' style={{opacity: 1 - style.opacity, transition: opacityTransition}}
+                 onClick={e => {if (e.target === e.currentTarget) fechar();}}
+                 onWheel={e => setScrollOutside(s => s + e.deltaY)}>
+                <div>
+                    <div className='closing-x' onClick={fechar}/>
+                    <div className='project-info-container'>
+                        <ProjectCarrossel project={project} isVertical={isVertical}/>
+                        <ProjectTextColumn project={project} isVertical={isVertical}/>
+                    </div>
                 </div>
             </div>
         </div>
